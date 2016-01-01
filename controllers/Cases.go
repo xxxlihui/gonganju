b6008e581e70931de838c017fe94d529b6950211
li <cabopkll@sina.com>
2016-01-01 16:38:16 +0800

    界面大概完成

--- controllers/Cases.go
+++ controllers/Cases.go
@@ -2,19 +2,29 @@ package controllers
 type Cases struct {
 	Controller
 }
-func (c *Cases)List(){
-c.TplNames="cases/list.html"
+
+func (c *Cases)Assigned(){
+	c.Data["Paths"] = [...] paths{{Name:"案件指派"}}
+	c.Data["PanelName"] = "案件列表"
+	c.Data["PanelTitle"] = "案件指派列表"
+	c.TplNames = "cases/assigned.html"
 }
-func (c* Cases)Add(){
-	c.TplNames="cases/add.html"
+func (c *Cases)List() {
+	c.Data["Paths"] = [...] paths{{Name:"案件指派"}}
+	c.Data["PanelName"] = "案件列表"
+	c.Data["PanelTitle"] = "案件指派列表"
+	c.TplNames = "cases/list.html"
 }
-func (c* Cases)Edit(){
-c.TplNames="cases/edit.html"
+func (c* Cases)Add() {
+	c.TplNames = "cases/add.html"
 }
-func (c* Cases)Insert(){
+func (c* Cases)Edit() {
+	c.TplNames = "cases/edit.html"
+}
+func (c* Cases)Insert() {
 
 }
-func (c* Cases)Update(){
+func (c* Cases)Update() {
 
 }
 
--- controllers/PoliceInfo.go
+++ controllers/PoliceInfo.go
@@ -4,5 +4,7 @@ type PoliceInfo struct {
 }
 func (c *PoliceInfo)Index(){
 	c.Data["Paths"]=[...] paths {{Name:"个人信息统计"}}
+	c.Data["PanelName"]="个人统计"
+	c.Data["PanelTitle"]=c.Police.PoliceName+"的统计信息"
 	c.TplNames="police/policeinfo.html"
 }
